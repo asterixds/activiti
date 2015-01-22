a6b956dcd1c795bd151067f7bc42e2b57929985d
Joram Barrez <joram.barrez@gmail.com>
2015-01-22 17:26:20 +0100

    Using basic auth for sending events

--- modules/activiti-engine/src/main/java/com/activiti/addon/cluster/AdminAppService.java
+++ modules/activiti-engine/src/main/java/com/activiti/addon/cluster/AdminAppService.java
@@ -12,22 +12,19 @@
  */
 package com.activiti.addon.cluster;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.client.HttpClient;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.methods.RequestBuilder;
-import org.apache.http.cookie.Cookie;
 import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,8 +48,6 @@ public class AdminAppService {
 	protected String userName;
 	protected String password;
 	
-	protected Cookie cookie;
-	
 	public AdminAppService() {
 		
 	}
@@ -74,10 +69,10 @@ public class AdminAppService {
     	return false;
     }
 		
-		CloseableHttpClient client = getAuthenticatedClient();
+		CloseableHttpClient client = getHttpClient();
 		try {
 
-			HttpPost post = new HttpPost(url + "/app/rest/" + clusterName + "/events");
+			HttpPost post = new HttpPost(url + "/api/enterprise/" + clusterName + "/events");
 			post.setHeader("Content-Type", "application/json");
 			HttpEntity entity = new ByteArrayEntity(eventsString.getBytes("UTF-8"));
 			post.setEntity(entity);
@@ -109,84 +104,12 @@ public class AdminAppService {
 		return true;
 	}
 	
-	/**
-	 * Creates an authenticated {@link HttpClient} to use when connecting to the Admin App. 
-	 */
-	protected CloseableHttpClient getAuthenticatedClient() {
-		
-		// Build session
-		BasicCookieStore cookieStore = new BasicCookieStore();
-		CloseableHttpClient httpClient = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
-
-		// If cookie wasn't provided in the cookie string, try to get a new cookie by logging in
-		if (cookie == null) {
-			try {
-				HttpUriRequest login = RequestBuilder.post()
-				    .setUri(new URI(url + "/app/authentication"))
-				    .addParameter("j_username", userName)
-				    .addParameter("j_password", password)
-				    .addParameter("_spring_security_remember_me", "true").build();
-
-				CloseableHttpResponse response = httpClient.execute(login);
-
-				try {
-					EntityUtils.consume(response.getEntity());
-					List<Cookie> cookies = cookieStore.getCookies();
-					if (cookies.isEmpty()) {
-						// nothing to do
-					} else {
-						this.cookie = cookies.get(0);
-					}
-
-				} finally {
-					response.close();
-				}
-
-			} catch (Exception e) {
-				logger.error("Error authenticating " + userName, e);
-			}
-
-		} else {
-			// setting cookie from cache
-			cookieStore.addCookie(cookie);
-		}
-
-		return httpClient;
-	}
-
-	/**
-	 * Little helper method that converts hex values from strings to byte array.
-	 *
-	 * @param hexString
-	 *          string of hex-encoded values
-	 * @return decoded byte array
-	 */
-	protected byte[] hexStringToByteArray(String hexString) {
-		int len = hexString.length();
-		byte[] data = new byte[len / 2];
-		for (int i = 0; i < len; i += 2) {
-			data[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4) + Character
-			    .digit(hexString.charAt(i + 1), 16));
-		}
-		return data;
+	public CloseableHttpClient getHttpClient() {
+		CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(userName, password));
+    return HttpClientBuilder.create().setDefaultCredentialsProvider(credentialsProvider).build();
 	}
 	
-	/**
-	 * Using some super basic byte array &lt;-&gt; hex conversions so we don't
-	 * have to rely on any large Base64 libraries. 
-	 */
-	protected String byteArrayToHexString(byte[] bytes) {
-		StringBuilder sb = new StringBuilder(bytes.length * 2);
-		for (byte element : bytes) {
-			int v = element & 0xff;
-			if (v < 16) {
-				sb.append('0');
-			}
-			sb.append(Integer.toHexString(v));
-		}
-		return sb.toString().toUpperCase();
-	}
-
 	public void setUrl(String url) {
 		this.url = url;
 	}
